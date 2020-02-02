99c864d8f65f625432f1fa1b508c8c4d03c22ea8
T S <taneymail@gmail.com>
2020-02-02 23:06:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,15 +2,14 @@ class Dog
 end
 
 def bark
-    puts "Woof!"
+  puts "Woof!"
 end
 
 
 fido = Dog.new
 fido.sit 
 
-class Dog
-    def sit
-        puts "The Dog is sitting"
-    end
+
+def sit
+  puts "The Dog is sitting"
 end
